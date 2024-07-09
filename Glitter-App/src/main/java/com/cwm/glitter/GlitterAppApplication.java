e4dd7b8ad72490dea6caa191f79733ecd292f2ea
Bhosale <mayur.b.bhosale@capgemini.com>
2024-07-09 10:39:15 +0530

    Created Backed end Glitter application

---
+++ Glitter-App/src/main/java/com/cwm/glitter/GlitterAppApplication.java
@@ -0,0 +1,13 @@
+package com.cwm.glitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlitterAppApplication.class, args);
+	}
+
+}
---
+++ Glitter-App/src/test/java/com/cwm/glitter/GlitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.glitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlitterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
